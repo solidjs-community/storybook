59d38a986c1d3c0ad29e024dbff9a7860901ab07
Maksim Kachurin <kachurun@gmail.com>
2025-08-09 22:06:16 -0700

    Add portable stories support and mount API for Solid (#7)
    Introduces a new mount API for Solid stories, adds support for portable stories in the renderer, and updates entry-preview to export mount and applyDecorators. Also improves global environment setup and updates dependencies for compatibility.

--- src/renderer/globals.ts
+++ src/renderer/globals.ts
@@ -2,4 +2,6 @@ import { global } from '@storybook/global';
 
 const { window: globalWindow } = global as any;
 
-globalWindow.STORYBOOK_ENV = 'solid';
+if (globalWindow) {
+    globalWindow.STORYBOOK_ENV = 'solid';
+}
